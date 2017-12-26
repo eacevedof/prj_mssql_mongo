67b5071ddfe841daf7bee1e1d9252120b882a0da
eacevedof <eacevedof@yahoo.es>
2017-12-26 12:04:53 +0100

    sigue recuperando todo

--- models/model_accprod_loop.js
+++ models/model_accprod_loop.js
@@ -1,37 +1,37 @@
-var mongoose = require('mongoose'),  
-db = mongoose.createConnection('localhost', 'flamagas');
+const mongoose = require("mongoose"),  
+      db = mongoose.createConnection('localhost','flamagas')
 
-db.on('error', console.error.bind(console, 'connection error:'));  
+db.on('error', console.error.bind(console, 'connection error:'))
 
 const get_list = function(codSO, callback) {  
     db.once('open', function() {
         console.log("opened!!")
-      var oSchema = new mongoose.Schema({
-        code: String,
-        code_sales_org: String
-      });
-      var Team = db.model('products', oSchema);
-      Team.find({code_sales_org:codSO},function(oError, arRows) {
-        if (oError) {
-          on_error(oError, callback);
-        } else {
-          mongoose.connection.close();
-          console.log(arRows);
-          callback("", arRows);
-        }
-      }); // end Team.find 
-    }); // end db.once open 
-  };
+        let oSchema = new mongoose.Schema({
+            code: String,
+            code_sales_org: String
+        })
+        let oModel = db.model('products', oSchema);
+        oModel.find({code_sales_org:codSO},function(oError, arRows) {
+            if (oError) {
+                on_error(oError, callback);
+            } else {
+                mongoose.connection.close();
+                console.log(arRows);
+                callback("", arRows);
+            }
+        }) // end oModel.find 
+    }) // end db.once open 
+}
 
+get_list("OVMN02",(e,d)=>{
+    ;
+})
 
-  get_list("OVTN02",(e,d)=>{
-      ;
-  })
-
-  const on_error = function(oError,callback) {  
+const on_error = function(oError,callback) {  
     mongoose.connection.close();
     callback(oError);
-  };
+}
+
 /*
 SELECT '{code_user: "'+Code_User+'", '+
 +'code_sales_org: "'+Code_Sales_Org+'", '
