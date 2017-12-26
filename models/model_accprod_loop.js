f77fd077d268529e218fff615880006c37024bf0
eacevedof <eacevedof@yahoo.es>
2017-12-26 11:52:29 +0100

    al menos lista products

--- models/model_accprod_loop.js
+++ models/model_accprod_loop.js
@@ -1,3 +1,37 @@
+var mongoose = require('mongoose'),  
+db = mongoose.createConnection('localhost', 'flamagas');
+
+db.on('error', console.error.bind(console, 'connection error:'));  
+
+const teamlist = function(gname, callback) {  
+    db.once('open', function() {
+        console.log("opened!!")
+      var teamSchema = new mongoose.Schema({
+        code: String,
+        code_sales_org: String
+      });
+      var Team = db.model('products', teamSchema);
+      Team.find({},function(err, teams) {
+        if (err) {
+          onErr(err, callback);
+        } else {
+          mongoose.connection.close();
+          console.log(teams);
+          callback("", teams);
+        }
+      }); // end Team.find 
+    }); // end db.once open 
+  };
+
+
+  teamlist("a",(e,d)=>{
+      ;
+  })
+
+  var onErr = function(err,callback) {  
+    mongoose.connection.close();
+    callback(err);
+  };
 /*
 SELECT '{code_user: "'+Code_User+'", '+
 +'code_sales_org: "'+Code_Sales_Org+'", '
