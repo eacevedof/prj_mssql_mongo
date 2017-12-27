3ae036cb7b516d297fa3be1f0cb4e48b26d41a92
eacevedof <eacevedof@yahoo.es>
2017-12-27 13:05:03 +0100

    cargo todo como objetos

--- scripts/scp_pricing.js
+++ scripts/scp_pricing.js
@@ -17,15 +17,29 @@ oAsync.parallel({
         accprod : function(fnCallback){oAccProd.get_documents(fnCallback)},
         acc : function(fnCallback){oAcc.get_documents("OVMN02",fnCallback)},
         prod: function(fnCallback){oProd.get_documents("OVMN02",fnCallback)},
-        //struct: function(fnCallback){oStruct.get_documents(fnCallback)},
-        //prior: function(fnCallback){oPrior.get_documents(fnCallback)},
-        //conds: function(fnCallback){oConds.get_documents(fnCallback)}
+
+        prior: function(fnCallback){oPrior.get_documents(fnCallback)},        
+        struct: function(fnCallback){oStruct.get_documents(fnCallback)},
+        conds: function(fnCallback){oConds.get_documents(fnCallback)}
     },function(oError,oR){
-        console.log("result in parallel")
-        let arAccounts = oPrice.account.get_minified(oR.acc,oPrice.account.get_unique(oR.accprod))
-        let arProducts = oPrice.product.get_minified(oR.prod,oPrice.product.get_unique(oR.accprod))
+        console.log("\nresult in parallel")
+
+        let arLoop = oR.accprod.map(o => o.toObject())
+        let arAccounts = oPrice.account.get_minified(oR.acc, oPrice.account.get_unique(oR.accprod))
+        let arProducts = oPrice.product.get_minified(oR.prod, oPrice.product.get_unique(oR.accprod))
+        
+        //pricing
+        let arPriority = oR.prior.map(o => o.toObject())
+        let arStructure = oR.struct.map(o => o.toObject())
+        let arConds = oR.conds.map(o => o.toObject())
+
+        arLoop.forEach(o => {
+            
+        });
+
         //let oAcc = oPrice.account.get_vars(arAccounts,"451","OVMN02")
-        console.log("accs:",arAccounts.length,",prod:",arProducts.length)
-        console.log("process time:",process.uptime())
+        //let oProd = oPrice.account.get_vars(arProducts,"451","OVMN02")
+        //console.log("accs:",arAccounts.length,",prod:",arProducts.length)
+        //console.log("process time:",process.uptime())
         process.exit()
 });
