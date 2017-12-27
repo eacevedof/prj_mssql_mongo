06dc29f51c16767995586ab967181ac874954a46
eacevedof <eacevedof@yahoo.es>
2017-12-27 08:56:55 +0100

    cargando datos en arrays

--- scripts/scp_pricing.js
+++ scripts/scp_pricing.js
@@ -8,11 +8,13 @@ const oAccProd = require("../models/model_accprods")
 const arAll = []
 
 oAsync.parallel([
-        (cb)=>{oAccProd.get_documents(cb)},
-        (cb)=>{oAcc.get_documents("OVMN02",cb)},
-        (cb)=>{oProd.get_documents("OVMN02",cb)}
+    {
+        a:(cb)=>{oAccProd.get_documents(cb)},
+        b:(cb)=>{oAcc.get_documents("OVMN02",cb)},
+        c:(cb)=>{oProd.get_documents("OVMN02",cb)}
+    }
     ],function(oError,arData){
-        console.log("arData[0]:",arData[0].length)
+        console.log("arData:",arData)
         let arAcc = arData[0]
         let arPrd = arData[1]
         console.log(arAcc.length,arPrd.length)
